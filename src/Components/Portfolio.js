d16c5785e6c1a9aa8a1a69d86657e429be19fcdd
leander006 <leanderdsilva06@gmail.com>
2022-10-27 18:59:00 +0530

    portfolio part updated

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,43 +4,46 @@ function Portfolio() {
   const portfolio = [
     {
       id: 1,
-      title: "Social-Media-Website",
+      title: "Social Media Website",
       src: "Images/social.png",
       href: "https://github.com/leander006/social-media-app",
     },
     {
       id: 2,
-      title: "News-Api-website",
+      title: "News website",
       src: "Images/news.png",
       href: "https://github.com/leander006/News-App",
     },
     {
       id: 3,
-      title: "Dog-Service-website",
+      title: "Dog Service website",
       src: "Images/dog.png",
       href: "https://github.com/leander006/Dog-service-website",
     },
     {
       id: 4,
-      title: "Quora-website",
+      title: "Quora website",
       src: "Images/quora.png",
       href: "https://github.com/leander006/Forum-discussion",
     },
     {
       id: 5,
-      title: "Blog-website",
+      title: "Blog website",
       src: "Images/blog.png",
       href: "https://github.com/leander006/Blog-frontend",
     },
     {
       id: 6,
-      title: "Portfolio-website",
+      title: "Portfolio website",
       src: "Images/portfolio.png",
       href: "https://github.com/leander006/Portfolio",
     },
   ];
   return (
-    <div name="portfolio" className="w-screen flex flex-col justify-center pt-12 md:h-screen">
+    <div
+      name="portfolio"
+      className="w-screen flex flex-col justify-center pt-12 md:h-screen"
+    >
       <div className="text-5xl mx-6 pb-3 lg:mx-24 font-WorkSans font-bold text-blue-500 ">
         PORTFOLIO
       </div>
@@ -58,9 +61,11 @@ function Portfolio() {
               src={src}
               alt="portfolio"
             />
-            <div className="hidden group-hover:block absolute top-[35%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-              <h1 className="text-white text-lg font-bold my-2">{title}</h1>
-              <h1 className="text-black text-xl rounded-md bg-white w-fit p-2 ">
+            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+              <h1 className="text-white md:text-lg font-bold md:my-2">
+                {title}
+              </h1>
+              <h1 className="text-black md:text-xl rounded-md bg-white p-0.5 w-fit md:p-2 ">
                 <a href={href} target="_blank" rel="noreferrer">
                   Code
                 </a>
